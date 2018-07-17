5345a565245c95cf902c0020be141c6242d79869
casselsg <casselsg19@bcdschool.org>
2018-07-17 13:54:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 class App < Sinatra::Base
 
+class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World"
+  end
+
+
+
+
   # Define your GET '/' route below and respond with "Hello, World!"
 
 end
